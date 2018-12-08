13ec4431376165a4ac4d1106d4da6e509eebaec0
adadlani1 <45290647+adadlani1@users.noreply.github.com>
2018-12-08 19:40:15 +0000

    Add files via upload

--- HASKELL_coursework.hs
+++ HASKELL_coursework.hs
@@ -7,7 +7,7 @@ velocity x y = sqrt((9.81 * (x -y)) / 0.5)
 
 distance :: Point -> Point -> Float
 distance (x1 , y1) (x2 , y2) = sqrt (x'*x' + y'*y')
-	where
+    where
 		x' = x1 - x2
 		y' = y1 - y2
 
@@ -39,7 +39,7 @@ type Candidate = (Point, Point, [Point], Float)
 
 make_candidates :: Point -> Point -> [[Point]] -> [Candidate]
 make_candidates start_point end_point [] = []
-make_candidates start_point end_point (xs:xss) = [(s , e, xs, t s e xs )] ++ make_candidates s e xss
+make_candidates start_point end_point (xs:xss) = [(s, e, xs, t s e xs )] ++ make_candidates s e xss
 	where
 		s = start_point
 		e = end_point
@@ -52,11 +52,55 @@ sort_by_time (x:xs) = sort_by_time smaller ++ [x] ++ sort_by_time larger
 		smaller = [a | a <- xs, a <= x]
 		larger =  [b | b <- xs, b > x]
 		
+get_first :: (a,b,c,d) -> a
+get_first (x,_,_,_) = x
+
+get_second :: (a,b,c,d) -> b
+get_second (_,x,_,_) = x 
+
+get_third :: (a,b,c,d) -> c 
+get_third (_,_,x,_) = x 
+
+get_fourth :: (a,b,c,d) -> d
+get_fourth (_,_,_,x) = x
+		
+supporting_string :: [Point] -> String
+supporting_string [] = ""
+supporting_string (x:xs) = point_to_string x ++ " " ++  supporting_string xs
+
 candidate_to_string :: Candidate -> String
+candidate_to_string candidate = starting_string ++ supporting_string (get_third candidate) ++ last_string ++ time_string
+	where
+		starting_string = point_to_string (get_first candidate )		
+		last_string = point_to_string (get_second candidate)
+		time_string = "Time: " ++ show (get_fourth candidate)
+
+point_to_string :: Point -> String
+point_to_string coordinate = show (fst coordinate) ++ "" ++ show (snd coordinate) ++ "\n"
+
+divide_list :: [Float] -> [Float] -> [[Point]]
+divide_list xs ys | length xs < length ys = [zip xs (take (l) ys)] ++ divide_list (drop (l) ys)
+				  | length xs <= length ys = [zip xs ys]
+						where
+							l = length xs
+				  
 
 
 
 --Part 3--
+--random_list returns in a 2-tuple, a list of random Float numbers and a random number generator
+--this function takes in an integer which is the n number of randomly generated values
+--along with a desired range of numbers and a random number generator.
+--Takes them as an input 
+
+--random_list :: Int −> (Int, Int) −> StdGen −> ([Int], StdGen)
+--random_list 0 _ gen = ([], gen)
+--random_list n minmax gen = ((r:rs), g2)
+	--where
+		--( r , g) = randomR minmax gen
+		--( rs, g2) = random_list (n−1) minmax g
+		
+--create_random_candidates :: Int -> Point -> Point -> [Float] -> (Float,Float) -> StdGen -> ([Candidate], StdGen)
 
 
 --Part 4--
